e2a18f7589332e745d38424428a20bf49dbd7a50
praveen <praveen.kutharkar@gmail.com>
2025-05-10 19:10:37 +0530

    cicd-test- test-1

--- Function1.cs
+++ Function1.cs
@@ -18,7 +18,7 @@ namespace devops_demo
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!, run-1");
+            return new OkObjectResult("Welcome to Azure Functions!, run-2 CICD test-1");
         }
     }
 }
